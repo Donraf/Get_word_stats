eaefa57ec384ac80a9fe588edf8a4120172d7477
Maxim <maxim.anikin1@yandex.ru>
2022-04-19 16:05:39 +0300

    Code sanitizing

--- csvfile.py
+++ csvfile.py
@@ -9,74 +9,74 @@ from utils import log_line_file
 
 
 class CsvHandler:
-    search_time = 0
-    write_time = 0
+    def __init__(self, rus_set_path=FILE_RUS_WORDS):
+        if rus_set_path:
+            self.rus_words_set = set()
+            with open(rus_set_path, 'r', encoding=ENCODING) as file:
+                for line in file:
+                    self.rus_words_set.add(line.strip().lower())
+        else:
+            self.rus_words_set = set()
 
-    def _check_for_duplicate(self, file, word_dict):
+    _search_time = 0
+    _write_time = 0
+
+    def _check_for_duplicate(self, csv_file, words_in_doc):
         words = []
-        line_num = 0
-        for line in file:
-            line_num += 1
+        for line_num, line in enumerate(csv_file):
             word_csv = re.match(r'\w+', line).group()
-            if word_csv in word_dict:
+            if word_csv in words_in_doc and \
+                    word_csv in self.rus_words_set:
                 words.append({
                     WORD: word_csv,
-                    WORD_NUM_IN_DOC: word_dict[word_csv],
+                    WORD_NUM_IN_DOC: words_in_doc[word_csv],
                     FIRST_OCCURRENCE: False,
                     WORD_NUM_LINE: line_num,
                 })
-                del word_dict[word_csv]
-        for key in word_dict.keys():
+                del words_in_doc[word_csv]
+        for key in words_in_doc.keys():
             words.append({
                 WORD: key,
-                WORD_NUM_IN_DOC: word_dict[key],
+                WORD_NUM_IN_DOC: words_in_doc[key],
                 FIRST_OCCURRENCE: True,
-                WORD_NUM_LINE: 0,
+                WORD_NUM_LINE: -1,
             })
         return words
 
-    def add_dict(self, file_path, d: defaultdict):
+    def add_dict(self, csv_path, words_in_doc: defaultdict):
         start_time = time.time()
-        with open(file_path, "r", encoding=ENCODING) as file:
-            words = self._check_for_duplicate(file, d)
+
+        with open(csv_path, "r", encoding=ENCODING) as csv_file:
+            words = self._check_for_duplicate(csv_file, words_in_doc)
         words = sorted(words, key=itemgetter(FIRST_OCCURRENCE, WORD_NUM_LINE, WORD_NUM_IN_DOC))
+
         end_search_time = time.time()
         log_line_file(FILE_DUP, str(end_search_time - start_time))
-        print(f"Duplicate check time: {end_search_time - start_time}")
-        self.search_time += end_search_time - start_time
-        self._add_record(file_path, words)
+        self._search_time += end_search_time - start_time
+
+        self._add_record(csv_path, words)
+
         end_write_time = time.time()
         log_line_file(FILE_WRI, str(end_write_time - end_search_time))
-        print(f"Write to a file time: {end_write_time - end_search_time}")
-        self.write_time += end_write_time - end_search_time
-        try:
-            print(f"Total absolute time / Percentage:\n" +
-                  f"Duplicate check: {self.search_time} / {self.search_time / (self.search_time + self.write_time) * 100}%" +
-                  f"\nWrite to a file: {self.write_time} / {self.write_time / (self.search_time + self.write_time) * 100}%")
-        except:
-            pass
+        self._write_time += end_write_time - end_search_time
 
-    def _add_record(self, file_path, words):
-        temp_file_path = os.path.join(TEMP_CSV_DIR, r"temp_csv.txt")
-        if os.path.exists(temp_file_path):
-            os.remove(temp_file_path)
+    def _add_record(self, csv_path, words):
+        new_csv_path = os.path.join(TEMP_CSV_DIR, r"temp_csv.txt")
 
-        with open(temp_file_path, "a+", encoding=ENCODING) as temp_file, \
-                open(file_path, "r", encoding=ENCODING) as file:
+        with open(csv_path, "r", encoding=ENCODING) as csv_file, \
+                open(new_csv_path, "w+", encoding=ENCODING) as new_csv_file:
             word_index = 0
-            line_counter = 0
-            for line in file:
-                line_counter += 1
+            for line_num, line in enumerate(csv_file):
                 if word_index != len(words) and \
                     not words[word_index][FIRST_OCCURRENCE] and \
-                   words[word_index][WORD_NUM_LINE] == line_counter:
+                        words[word_index][WORD_NUM_LINE] == line_num:
                     num_csv = re.search(r"\d+", line).group()
-                    temp_file.write(f"{words[word_index][WORD]}" + CSV_DELIMITER +
-                                    f"{words[word_index][WORD_NUM_IN_DOC] + int(num_csv)}\n")
+                    new_csv_file.write(f"{words[word_index][WORD]}" + CSV_DELIMITER +
+                                       f"{words[word_index][WORD_NUM_IN_DOC] + int(num_csv)}\n")
                     word_index += 1
                 else:
-                    temp_file.write(line)
+                    new_csv_file.write(line)
             for index in range(word_index, len(words)):
-                temp_file.write(f"{words[index][WORD]}" + CSV_DELIMITER +
-                                f"{words[index][WORD_NUM_IN_DOC]}\n")
-        os.replace(temp_file_path, file_path)
+                new_csv_file.write(f"{words[index][WORD]}" + CSV_DELIMITER +
+                                   f"{words[index][WORD_NUM_IN_DOC]}\n")
+        os.replace(new_csv_path, csv_path)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import re
 import os
 import time
 import codecs
@@ -12,46 +11,40 @@ from utils import *
 
 
 csv_handler = CsvHandler()
-list_text_dir = os.listdir(TEXT_DIR)
+docs_list = os.listdir(TEXT_DIR)
 
-defined_encodings = defaultdict(int)
 total_enc_time = 0
-
 log_line_file(FILE_WTD, '0')
 
-
 file_counter = 0
-for text_file_name in list_text_dir:
+for doc_name in docs_list:
     start_file_proc = time.time()
-
     file_counter += 1
-    doc_word_dict = defaultdict(int)
-    text_file_path = os.path.join(TEXT_DIR, text_file_name)
-    temp_text_file_path = os.path.join(TEMP_ENCODING_DIR, text_file_name)
     print(f"File {file_counter}")
+
+    doc_path = os.path.join(TEXT_DIR, doc_name)
+
     # --- START Encoding check
     detector = UniversalDetector()
     print("Checking file's encoding")
     enc_start = time.time()
     try:
-        with open(text_file_path, "rb") as file:
-            for line in file:
+        with open(doc_path, "rb") as doc:
+            for line in doc:
                 detector.feed(line)
                 if detector.done:
                     break
         detector.close()
-        det_result = detector.result["encoding"]
-        defined_encodings[det_result] += 1
-        print(det_result)
-        if det_result != ENCODING:
-            wrong_encoding = det_result
-            with codecs.open(text_file_path, "rU", wrong_encoding) as wr_enc_file, \
-                    codecs.open(temp_text_file_path, "w+", ENCODING) as cor_enc_file:
-                for line in wr_enc_file:
-                    cor_enc_file.write(line)
-            text_file_path = temp_text_file_path
+        doc_encoding = detector.result["encoding"]
+        print(doc_encoding)
+        if doc_encoding != ENCODING:
+            enc_doc_path = os.path.join(TEMP_ENCODING_DIR, doc_name)
+            with codecs.open(doc_path, "rU", doc_encoding) as old_enc_doc, \
+                    codecs.open(enc_doc_path, "w+", ENCODING) as new_enc_doc:
+                for line in old_enc_doc:
+                    new_enc_doc.write(line)
+            doc_path = enc_doc_path
     except:
-        defined_encodings["Error"] += 1
         log_line_file(FILE_ENC, str(time.time() - enc_start))
         log_line_file(FILE_DUP, '-')
         log_line_file(FILE_WRI, '-')
@@ -62,19 +55,20 @@ for text_file_name in list_text_dir:
     log_line_file(FILE_ENC, str(time.time() - enc_start))
     total_enc_time += time.time() - enc_start
     # --- END Encoding check
-    with open(text_file_path, "r", encoding=ENCODING) as text_file:
-        for line in text_file:
-            processed_line = sanitize_line(line)
-            line_word_dict = get_words_dict(processed_line)
-            for key in line_word_dict.keys():
-                doc_word_dict[key] += line_word_dict[key]
-        log_line_file(FILE_WFL, str(len(doc_word_dict.keys())))
-        print(len(doc_word_dict.keys()))
-    csv_handler.add_dict(CSV_FILE, doc_word_dict)
 
-    with open(CSV_FILE, 'r', encoding='utf-8') as check_file:
+    words_in_doc = defaultdict(int)
+    with open(doc_path, "r", encoding=ENCODING) as doc:
+        for line in doc:
+            sanitized_line = sanitize_line(line)
+            words_in_line = get_words_dict(sanitized_line)
+            for key in words_in_line.keys():
+                words_in_doc[key] += words_in_line[key]
+        log_line_file(FILE_WFL, str(len(words_in_doc.keys())))
+    csv_handler.add_dict(CSV_FILE, words_in_doc)
+
+    with open(CSV_FILE, 'r', encoding='utf-8') as csv_file:
         counter = 0
-        for _ in check_file:
+        for _ in csv_file:
             counter += 1
         log_line_file(FILE_WTD, str(counter))
 
@@ -82,9 +76,7 @@ for text_file_name in list_text_dir:
     log_line_file(FILE_TOT, str(end_file_proc))
 
 # --- START Delete temp files
-list_temp_text_dir = os.listdir(TEMP_ENCODING_DIR)
-for temp_file_name in list_temp_text_dir:
-    os.remove(os.path.join(TEMP_ENCODING_DIR, temp_file_name))
+for temp_enc_file in os.listdir(TEMP_ENCODING_DIR):
+    os.remove(os.path.join(TEMP_ENCODING_DIR, temp_enc_file))
 # --- END Delete temp files
-print(defined_encodings)
 print(f"Total encoding time {total_enc_time}")
